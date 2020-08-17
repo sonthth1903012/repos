e82edd841c088bbc4e73867524771bb4b10279d0
sonthth1903012 <49936840+sonthth1903012@users.noreply.github.com>
2020-08-17 11:15:28 +0700

    ...

---
+++ DataBIndingExample/DataBIndingExample/Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBIndingExample.Models
+{
+    class TodoItem
+    {
+        public string Title { get; set; }
+    }
+}
---
+++ ObserableCollectionExample/ObserableCollectionExample/Models/Icon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Store.Preview.InstallControl;
+
+namespace ObserableCollectionExample.Models
+{
+    class Icon
+    {
+        public string IconPath { get; set; }
+    }
+}
