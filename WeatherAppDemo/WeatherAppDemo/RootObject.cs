34a351619c975afd03a9a49448b4e8e617a9aa90
sonthth1903012 <49936840+sonthth1903012@users.noreply.github.com>
2020-08-19 18:41:05 +0700

    WeatherAppDemo

---
+++ WeatherAppDemo/WeatherAppDemo/RootObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WeatherAppDemo
+{
+    public class RootObject
+    {
+        internal object main;
+        internal string name;
+        internal object weather;
+
+        public static implicit operator RootObject(APIManager.RootObject v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
