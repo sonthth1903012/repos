2deeb7989760cd2cda5f00058e99d1e42474ec57
sonthth1903012 <49936840+sonthth1903012@users.noreply.github.com>
2020-08-27 22:18:46 +0700

    SQLite Demo

---
+++ SQLiteDEMO/SQLiteDEMO/SQLite_Database.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLiteDEMO
+{
+    class SQLite_Database
+    {
+        string path =
+        Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path,
+        "db.sqlite");
+        SQLite.Net.SQLiteConnection conn = new SQLite.Net.SQLiteConnection(new
+        SQLite.Net.Platform.WinRT.SQLitePlatformWinRT(), path);
+
+        conn.CreateTable<Customer>(); 
+
+        conn.Insert(new Customer()
+        {
+            Name = textBox.Text, 
+            Age = textBoxl.Text
+        }); 
+
+        var query = conn.Table<Customer>();
+        string id = ""; string name = "";
+        string age = ""; 
+        
+        foreach (var message in query) { 
+            id = id + " " + message.Id; 
+            name = name + " " + message.Name; 
+            age = age + " " + message.Age; 
+
+    }
+}
